82017f96c230380c76db6da51bc1f1d9e3e728f8
Mihir Kumar <mihirpandey.13@gmail.com>
2019-08-23 17:42:19 +0530

    [MAJOR] Author relation with book added

--- src/schema/Book.ts
+++ src/schema/Book.ts
@@ -1,11 +1,28 @@
 import { GraphQLObjectType, GraphQLString, GraphQLID } from 'graphql';
 
+import { logger } from '@shared';
+import { AuthorDao } from '@daos';
+
+import { AuthorObjectType } from './Author';
+
+const Author = new AuthorDao();
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        author: {
+            type: AuthorObjectType,
+            async resolve(parent, args) {
+                try {
+                    return await Author.find(parent.authorID);
+                } catch (err) {
+                    logger.error(err.message, err);
+                }
+            },
+        },
     }),
 });
 
